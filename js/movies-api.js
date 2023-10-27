c9dc90b940f2d95a61f9f11f94f7c95957eec9ab
Anna <annasereno27@gmail.com>
2023-10-27 18:22:36 -0500

    added delete button

--- js/movies-api.js
+++ js/movies-api.js
@@ -20,44 +20,12 @@ function getMovies() {
 		.then(response => response.json())
 		.then(movies => {
 			console.log(movies);
-		displayMovies(movies) //maybe move this to main too
+		generateCard(movies) //maybe move this to main too
 		})
 }
 
 getMovies()//delete this later
 
-const movieCards = document.querySelector("#movie-cards-row") //move this to main
-// export
-function displayMovies (movies) {
-
-	movies.forEach( movie =>{
-		const div = document.createElement('div');
-		const image = document.createElement('img');
-		const name = document.createElement('h3');
-		const summary = document.createElement('p')
-		const edit = document.createElement('button');
-		const remove = document.createElement('button');
-
-		div.classList.add('card')
-		div.classList.add('col')
-		image.classList.add('card-img')
-
-		image.src = movie.posterURL
-		name.innerText = movie.title
-		summary.innerText = movie.movieSummary
-		edit.textContent = 'Edit'
-		remove.innerText = 'Delete'
-
-		div.appendChild(image)
-		div.appendChild(name)
-		div.appendChild(summary)
-		div.appendChild(edit)
-		div.appendChild(remove)
-
-		movieCards.appendChild(div)
-	})
-
-}
 
 
 function addMovie() {
@@ -75,6 +43,8 @@ function addMovie() {
 	}
 }
 
+
+
 function createMovie(movie) {
 	try {
 		const options = {
@@ -86,7 +56,7 @@ function createMovie(movie) {
 		}
 		return fetch(`${MOVIES_HOST}/movies`, options)
 			.then(response => response.json())
-			.then(movies => movie);
+			.then(movie => movie);
 	} catch(error) {
 		console.error(error);
 	}
@@ -107,4 +77,73 @@ async function updateMovie(movie) {
 	} catch(error) {
 		console.error(error);
 	}
+}
+
+export async function deleteMovie(id) {
+	try {
+		const options = {
+			method: "DELETE"
+		}
+		return fetch(`${MOVIES_HOST}/books/${id}`, options)
+			.then(response => response.json())
+			.then(movie => book);
+	} catch(error) {
+		console.error(error);
+	}
+}
+
+  /////////// MOVE THIS TO MAIN//////////////
+const addMovieBtn = document.querySelector("#add-new-movie-btn")
+const movieCards = document.querySelector("#movie-cards-row")
+
+addMovieBtn.addEventListener("click", (e)=>{
+	e.preventDefault()
+
+	//create a movie object from the form fields
+	let newMovie ={
+		title: document.querySelector("#title").value,
+		rating: document.querySelector("#rating").value,
+		genre: document.querySelector("#genre").value,
+		movieSummary: document.querySelector("#movieSummary").value
+	}
+
+	createMovie(newMovie)//adds movie to JSON
+	movies.push(newMovie)//pushes movie to array
+	///add something that says thanks for your submission after clicking add my movie
+
+})
+function generateCard (movies) {
+
+	movies.forEach( movie =>{
+		const div = document.createElement('div');
+		const image = document.createElement('img');
+		const name = document.createElement('h3');
+		const summary = document.createElement('p')
+		const edit = document.createElement('button');
+		// edit.addEventListener("click", editMovie)
+		const remove = document.createElement('button');
+		remove.addEventListener("click", removeMovieCard)
+
+		div.classList.add('card')
+		div.classList.add('col-4')
+		image.classList.add('card-img')
+
+		image.src = movie.posterURL
+		name.innerText = movie.title
+		summary.innerText = movie.movieSummary
+		edit.textContent = 'Edit'
+		remove.innerText = 'Remove'
+
+		div.appendChild(image)
+		div.appendChild(name)
+		div.appendChild(summary)
+		div.appendChild(edit)
+		div.appendChild(remove)
+
+		movieCards.appendChild(div)
+	})
+}
+function removeMovieCard(event){
+	event.target.parentElement.remove()
+
 }
